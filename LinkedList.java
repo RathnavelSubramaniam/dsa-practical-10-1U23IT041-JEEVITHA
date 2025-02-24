011cad3bae9103af908d302640fd89cbda08fee0
1U23IT041-JEEVITHA <139448874+1U23IT041-JEEVITHA@users.noreply.github.com>
2025-02-24 09:10:20 +0000

    Done10

--- LinkedList.java
+++ LinkedList.java
@@ -44,9 +44,25 @@ System.out.println("Linked List After Deleting at tail");
 list.displayList();
 sc.close();
     }
-public void deleteEnd()
+    public void deleteEnd()
     {
-        // TYPE YOUR CODE HERE
+        if(head == null)
+            System.out.println("List is empty");
+            else if(head.next==null)
+            head=null;
+            else
+            {
+                Node temp=head;
+                Node prev=head;
+                while(temp.next.next!=null)
+                {
+                    prev=prev.next;
+                    temp=temp.next;
+                }
+                temp=temp.next;
+prev.next=null;
+System.out.println("Deleted Element is:"+temp.data);
+            }
     }
 public void displayList() {
         Node current = head;
